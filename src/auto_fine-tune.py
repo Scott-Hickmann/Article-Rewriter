e94af598c577d088ba04ebb258d46a541f904b4b
Scott Hickmann <scotthickmann21@gmail.com>
2022-03-07 16:42:54 -0800

    Auto Fine Tune

--- src/auto_fine-tune.py
+++ src/auto_fine-tune.py
@@ -21,11 +21,11 @@ dataframe = pd.read_csv('data/annotated_data.csv', encoding='utf-8')
 train, val = train_test_split(dataframe, test_size=0.2, random_state=config.seed)
 
 def get_dataset(data):
-  md_source_with_markdown = [mdEncode(text) for text in data['source_with_markdown']]
+  md_target_with_markdown = [mdEncode(text) for text in data['target_with_markdown']]
   return CustomDataset({
-    "source": [md_data[0] for md_data in md_source_with_markdown],
-    "target": [mdEncode(text, md_source_with_markdown[i][1])[0] for i, text in enumerate(data['target_with_markdown'])],
-  }, summarizer.encode), md_source_with_markdown
+    "source": [md_data[0] for md_data in md_target_with_markdown],
+    "target": [md_data[0] for md_data in md_target_with_markdown],
+  }, summarizer.encode), md_target_with_markdown
 
 training_set, _ = get_dataset(train)
 val_set, md_val_source_with_markdown = get_dataset(val)
@@ -55,27 +55,8 @@ for epoch in range(config.train_epochs):
 torch.save(summarizer.model.state_dict(), 'models/main.pt')
 print()
 
-def decode_batch(md_generated, md_expected):
-  print(md_generated)
-  print(md_expected)
-  generated = []
-  expected = []
-  for a_md_generated, a_md_expected in zip(md_generated, md_expected):
-    expected_index = val_set.target.index(a_md_expected)
-    md_lookup = md_val_source_with_markdown[expected_index][1]
-    try:
-      generated.append(mdDecode(a_md_generated, md_lookup))
-      expected.append(mdDecode(a_md_expected, md_lookup))
-    except:
-      generated.append(a_md_generated)
-      expected.append(a_md_expected)
-  print(generated)
-  print(expected)
-  return generated, expected
-
 print('Generating paraphrases on our fine-tuned model for the validation dataset and saving it in a dataframe')
-md_generated, md_expected = summarizer.validate(val_loader)
-generated, expected = decode_batch(md_generated, md_expected)
+generated, expected = summarizer.validate(val_loader)
 final_df = pd.DataFrame({'generated': generated, 'expected': expected})
 final_df.to_csv('data/predictions.csv', encoding='utf-8', index=False)
 print('Output Files generated for review')
